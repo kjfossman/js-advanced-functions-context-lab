99e02351dd69615fa2e5ba7751e750e165300340
Kyle <kjfossman@gmail.com>
2021-05-18 09:28:44 -0800

    commit tree fc208edcd756bf93f5cf491ec8fdcc094bff6917

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ let createEmployeeRecords = (array) => {
    return array.map(e => createEmployeeRecord(e))
 }
 
-let createTimeInEvent = (dateStamp) => {
+let createTimeInEvent = function(dateStamp){
     let [date, hour] = dateStamp.split(' ')
-    
+
     this.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(hour, 10),
@@ -47,4 +47,45 @@ let createTimeInEvent = (dateStamp) => {
     })
 
     return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date, 
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(date) {
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === date 
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === date 
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(date) {
+    let rWage = hoursWorkedOnDate.call(this, date) * this.payPerHour 
+    return parseInt(rWage, 10)
+}
+
+let findEmployeeByFirstName = function(employees, firstName) {
+    return employees.find(function(e){
+       return e.firstName === firstName
+    })     
+}
+
+let calculatePayroll = function(employees){
+    return employees.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
 }
